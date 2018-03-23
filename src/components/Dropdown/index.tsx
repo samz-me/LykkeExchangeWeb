b0bb019588d3874ed3f5d54b595b3bbb989293b4
Andrey Zhylin <andrey.zhylin@outlook.com>
2018-03-23 13:44:54 +0300

    Merge fixes

--- src/components/Dropdown/index.tsx
+++
@@ -1,7 +0,0 @@
-import './style.css';
-
-export {default as Dropdown, DropdownPosition} from './dropdown';
-export {default as DropdownContainer} from './dropdownContainer';
-export {default as DropdownControl} from './dropdownControl';
-export {default as DropdownList} from './dropdownList';
-export {default as DropdownListItem} from './dropdownListItem';
